dagronf/DSFSparkline

commit d431c445f9854d4ba61052f83ffa220596af73c4

Fixed some swiftui issues

Sources/DSFSparkline/private/ui/DSFSparklineDotGraphView+Private.swift
@@ -63,7 +63,6 @@ private extension DSFSparklineDotGraphView {
 
 		var position = drawRect.width - dotHeight - xOffset
 
-		let path = CGMutablePath()
 		let unsetPath = CGMutablePath()
 
 		// Map normalized values to box positions
@@ -71,6 +70,9 @@ private extension DSFSparklineDotGraphView {
 			let floatBoxPos = CGFloat(self.verticalDotCount) * dataPoint
 			return UInt(floatBoxPos.rounded(.awayFromZero))
 		}
+
+		var pv: [CGRect] = []
+		var uv: [CGRect] = []
 
 		for dataPoint in normalizedBoxed {
 			let boxCount = dataPoint
@@ -83,10 +85,12 @@ private extension DSFSparklineDotGraphView {
 				let ri = r.insetBy(dx: 0.5, dy: 0.5)
 
 				if c < boxCount {
-					path.addRect(ri)
+					pv.append(ri)
+					//path.addRect(ri)
 				}
 				else {
-					unsetPath.addRect(ri)
+					uv.append(ri)
+					//unsetPath.addRect(ri)
 				}
 			}
 
@@ -97,16 +101,24 @@ private extension DSFSparklineDotGraphView {
 			}
 		}
 
-		primary.usingGState { (state) in
-			state.addPath(path)
-			state.setFillColor(self.graphColor.cgColor)
-			state.drawPath(using: .fill)
+		if pv.count > 0 {
+			primary.usingGState { (state) in
+				let path = CGMutablePath()
+				path.addRects(pv)
+				state.addPath(path)
+				state.setFillColor(self.graphColor.cgColor)
+				state.drawPath(using: .fill)
+			}
 		}
 
-		primary.usingGState { (state) in
-			state.addPath(unsetPath)
-			state.setFillColor(self.unsetGraphColor.cgColor)
-			state.drawPath(using: .fill)
+		if uv.count > 0 {
+			primary.usingGState { (state) in
+				let path = CGMutablePath()
+				path.addRects(uv)
+				state.addPath(unsetPath)
+				state.setFillColor(self.unsetGraphColor.cgColor)
+				state.drawPath(using: .fill)
+			}
 		}
 	}
 

Demos/Demos.xcodeproj/project.pbxproj
@@ -3,7 +3,7 @@
 	archiveVersion = 1;
 	classes = {
 	};
-	objectVersion = 51;
+	objectVersion = 52;
 	objects = {
 
 /* Begin PBXAggregateTarget section */
@@ -52,11 +52,13 @@
 		23102BFE23B02B33001A621D /* LaunchScreen.storyboard in Resources */ = {isa = PBXBuildFile; fileRef = 23102BFC23B02B33001A621D /* LaunchScreen.storyboard */; };
 		23102C0423B03315001A621D /* cpuUsage.swift in Sources */ = {isa = PBXBuildFile; fileRef = 23102C0323B03315001A621D /* cpuUsage.swift */; };
 		231D4AAE25C10F2E0004F053 /* UnrangedContentView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 231D4AAD25C10F2E0004F053 /* UnrangedContentView.swift */; };
+		23229BDD25D7936B0000C2C6 /* ZeroLineConfigurationView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 23229BDC25D7936B0000C2C6 /* ZeroLineConfigurationView.swift */; };
 		2329C5A425D5C5DA000B500D /* LineGraphContentView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 2329C5A325D5C5DA000B500D /* LineGraphContentView.swift */; };
 		232F732323B5BE2A00FA3A3F /* GridViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 232F732223B5BE2A00FA3A3F /* GridViewController.swift */; };
 		2331FB5E25BFBA4600CE4311 /* WinLossGraphContentView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 2331FB5D25BFBA4600CE4311 /* WinLossGraphContentView.swift */; };
 		23369EB125D6601500A40023 /* DataBarGraphContentView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 23369EB025D6601500A40023 /* DataBarGraphContentView.swift */; };
 		2352E6F925BFCE8000321175 /* OverlayView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 2352E6F825BFCE8000321175 /* OverlayView.swift */; };
+		235623D825D6BE2C007E5CF7 /* BarGraphBuilderContentView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 235623D725D6BE2C007E5CF7 /* BarGraphBuilderContentView.swift */; };
 		236A57FB25C1392B00F2BACF /* SwiftUI_Sparkline_DemoApp.swift in Sources */ = {isa = PBXBuildFile; fileRef = 236A57E925C1392A00F2BACF /* SwiftUI_Sparkline_DemoApp.swift */; };
 		236A57FC25C1392B00F2BACF /* SwiftUI_Sparkline_DemoApp.swift in Sources */ = {isa = PBXBuildFile; fileRef = 236A57E925C1392A00F2BACF /* SwiftUI_Sparkline_DemoApp.swift */; };
 		236A57FD25C1392B00F2BACF /* ContentView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 236A57EA25C1392A00F2BACF /* ContentView.swift */; };
@@ -74,6 +76,7 @@
 		23E2278E23B5846E00E59122 /* ViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 23E2278D23B5846E00E59122 /* ViewController.swift */; };
 		23E2279023B5846E00E59122 /* Assets.xcassets in Resources */ = {isa = PBXBuildFile; fileRef = 23E2278F23B5846E00E59122 /* Assets.xcassets */; };
 		23E2279323B5846E00E59122 /* Main.storyboard in Resources */ = {isa = PBXBuildFile; fileRef = 23E2279123B5846E00E59122 /* Main.storyboard */; };
+		23F7646B25D6C4C400E63649 /* DSFStepperView in Frameworks */ = {isa = PBXBuildFile; productRef = 23F7646A25D6C4C400E63649 /* DSFStepperView */; };
 		4079C8A3550FDF1D8564108E /* Pods_macOS_SwiftUI_Demo.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 0893A7B69A2C716F5E308880 /* Pods_macOS_SwiftUI_Demo.framework */; };
 		56BB7F1533CC30E57AC9654A /* Pods_SwiftUI_Sparkline_Demo__iOS_.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 6AB4E75AB431D5E8C3C582F2 /* Pods_SwiftUI_Sparkline_Demo__iOS_.framework */; };
 		73E144093BF355D884B0ACAB /* Pods_iOS_Sparkline_Demo.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 462EC212C63FCA74CCCDC138 /* Pods_iOS_Sparkline_Demo.framework */; };
@@ -170,11 +173,13 @@
 		23102BFF23B02B33001A621D /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
 		23102C0323B03315001A621D /* cpuUsage.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = cpuUsage.swift; sourceTree = "<group>"; };
 		231D4AAD25C10F2E0004F053 /* UnrangedContentView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = UnrangedContentView.swift; sourceTree = "<group>"; };
+		23229BDC25D7936B0000C2C6 /* ZeroLineConfigurationView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ZeroLineConfigurationView.swift; sourceTree = "<group>"; };
 		2329C5A325D5C5DA000B500D /* LineGraphContentView.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = LineGraphContentView.swift; sourceTree = "<group>"; };
 		232F732223B5BE2A00FA3A3F /* GridViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = GridViewController.swift; sourceTree = "<group>"; };
 		2331FB5D25BFBA4600CE4311 /* WinLossGraphContentView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = WinLossGraphContentView.swift; sourceTree = "<group>"; };
 		23369EB025D6601500A40023 /* DataBarGraphContentView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = DataBarGraphContentView.swift; sourceTree = "<group>"; };
 		2352E6F825BFCE8000321175 /* OverlayView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = OverlayView.swift; sourceTree = "<group>"; };
+		235623D725D6BE2C007E5CF7 /* BarGraphBuilderContentView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = BarGraphBuilderContentView.swift; sourceTree = "<group>"; };
 		236A57E925C1392A00F2BACF /* SwiftUI_Sparkline_DemoApp.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SwiftUI_Sparkline_DemoApp.swift; sourceTree = "<group>"; };
 		236A57EA25C1392A00F2BACF /* ContentView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ContentView.swift; sourceTree = "<group>"; };
 		236A57EB25C1392B00F2BACF /* Assets.xcassets */ = {isa = PBXFileReference; lastKnownFileType = folder.assetcatalog; path = Assets.xcassets; sourceTree = "<group>"; };
@@ -280,6 +285,7 @@
 			buildActionMask = 2147483647;
 			files = (
 				4079C8A3550FDF1D8564108E /* Pods_macOS_SwiftUI_Demo.framework in Frameworks */,
+				23F7646B25D6C4C400E63649 /* DSFStepperView in Frameworks */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -451,6 +457,8 @@
 				231D4AAD25C10F2E0004F053 /* UnrangedContentView.swift */,
 				23E20E6425D6178D001E8E67 /* PieGraphContentView.swift */,
 				23369EB025D6601500A40023 /* DataBarGraphContentView.swift */,
+				235623D725D6BE2C007E5CF7 /* BarGraphBuilderContentView.swift */,
+				23229BDC25D7936B0000C2C6 /* ZeroLineConfigurationView.swift */,
 			);
 			path = "macOS SwiftUI Demo";
 			sourceTree = "<group>";
@@ -634,6 +642,9 @@
 			dependencies = (
 			);
 			name = "macOS SwiftUI Demo";
+			packageProductDependencies = (
+				23F7646A25D6C4C400E63649 /* DSFStepperView */,
+			);
 			productName = "SwiftUI Demo";
 			productReference = 23882CDE257D675700AC103A /* macOS SwiftUI Demo.app */;
 			productType = "com.apple.product-type.application";
@@ -704,6 +715,9 @@
 				Base,
 			);
 			mainGroup = 23102B4423B0165B001A621D;
+			packageReferences = (
+				23F7646925D6C4C400E63649 /* XCRemoteSwiftPackageReference "DSFStepperView" */,
+			);
 			productRefGroup = 23102B5123B0245A001A621D /* Products */;
 			projectDirPath = "";
 			projectRoot = "";
@@ -1176,12 +1190,14 @@
 			files = (
 				23369EB125D6601500A40023 /* DataBarGraphContentView.swift in Sources */,
 				23882CE3257D675700AC103A /* ContentView.swift in Sources */,
+				23229BDD25D7936B0000C2C6 /* ZeroLineConfigurationView.swift in Sources */,
 				2352E6F925BFCE8000321175 /* OverlayView.swift in Sources */,
 				23E20E8125D618E3001E8E67 /* PieGraphContentView.swift in Sources */,
 				231D4AAE25C10F2E0004F053 /* UnrangedContentView.swift in Sources */,
 				238EF46425C0D88D00FCC829 /* StackLineGraphContentView.swift in Sources */,
 				2331FB5E25BFBA4600CE4311 /* WinLossGraphContentView.swift in Sources */,
 				23882CE1257D675700AC103A /* AppDelegate.swift in Sources */,
+				235623D825D6BE2C007E5CF7 /* BarGraphBuilderContentView.swift in Sources */,
 				2329C5A425D5C5DA000B500D /* LineGraphContentView.swift in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
@@ -2583,6 +2599,25 @@
 			defaultConfigurationName = Release;
 		};
 /* End XCConfigurationList section */
+
+/* Begin XCRemoteSwiftPackageReference section */
+		23F7646925D6C4C400E63649 /* XCRemoteSwiftPackageReference "DSFStepperView" */ = {
+			isa = XCRemoteSwiftPackageReference;
+			repositoryURL = "https://github.com/dagronf/DSFStepperView";
+			requirement = {
+				kind = upToNextMajorVersion;
+				minimumVersion = 1.1.4;
+			};
+		};
+/* End XCRemoteSwiftPackageReference section */
+
+/* Begin XCSwiftPackageProductDependency section */
+		23F7646A25D6C4C400E63649 /* DSFStepperView */ = {
+			isa = XCSwiftPackageProductDependency;
+			package = 23F7646925D6C4C400E63649 /* XCRemoteSwiftPackageReference "DSFStepperView" */;
+			productName = DSFStepperView;
+		};
+/* End XCSwiftPackageProductDependency section */
 	};
 	rootObject = 23102B4523B0165B001A621D /* Project object */;
 }

Demos/macOS SwiftUI Demo/BarGraphBuilderContentView.swift
@@ -1,0 +1,136 @@
+//
+//  BarGraphBuilderContentView.swift
+//  macOS SwiftUI Demo
+//
+//  Created by Darren Ford on 13/2/21.
+//
+
+import SwiftUI
+
+import DSFSparkline
+import DSFStepperView
+
+struct BarGraphBuilderContentView: View {
+
+	var BarDataSource: DSFSparklineDataSource = {
+		let d = DSFSparklineDataSource(windowSize: 20, range: 0 ... 1, zeroLineValue: 0.4)
+		d.push(values: [
+					0.85, 0.04, 0.24, 0.13, 0.51, 0.93, 0.26, 0.69, 0.16, 0.39,
+					0.19, 0.12, 0.28, 0.42, 0.42, 0.48, 0.29, 0.05, 0.87, 0.28
+		])
+		return d
+	}()
+
+
+	@State var showZeroLine: Bool = true
+	@State var lineWidth: CGFloat? = 1
+	@State var barWidth: CGFloat? = 1
+
+	@State var centerAroundZeroLine: Bool = false
+
+	@State var zeroLineValue: CGFloat? = 0.5
+
+	@State var graphColor: Color = Color(.sRGB, red: 0, green: 1, blue: 0)
+	@State var lowerGraphColor: Color = Color(.sRGB, red: 1, green: 0, blue: 0)
+
+	@State var zerolineColor: Color = Color(.sRGB, red: 0.5, green: 0.5, blue: 0.5)
+	@State var zerolineWidth: CGFloat? = 1
+
+	@State var zeroLineDashStyle: [CGFloat] = [1, 1]
+
+	var formatter: NumberFormatter = {
+		let format = NumberFormatter()
+		format.numberStyle = .decimal
+
+		// Always display a single digit fractional value.
+		format.allowsFloats = true
+		format.minimumFractionDigits = 1
+		format.maximumFractionDigits = 1
+
+		return format
+	}()
+
+	var body: some View {
+		VStack {
+			HStack(spacing: 20) {
+				Toggle("Show Zero Line", isOn: $showZeroLine)
+				Toggle("Center Around Zero Line", isOn: $centerAroundZeroLine)
+			}
+
+			HStack(spacing: 4) {
+				Text("Zero-line value")
+				DSFStepperView.SwiftUI(
+					configuration: DSFStepperView.SwiftUI.DisplaySettings(minimum: 0, maximum: 1, increment: 0.1, numberFormatter: formatter),
+					floatValue: $zeroLineValue,
+					onValueChange: { newVal in
+						if let n = newVal {
+							BarDataSource.zeroLineValue = n
+						}
+					}
+				)
+				.frame(width: 100, height: 24)
+			}
+
+			HStack {
+				ColorPicker("graphColor", selection: $graphColor)
+				ColorPicker("lowerGraphColor", selection: $lowerGraphColor)
+			}
+
+			HStack(spacing: 4) {
+				Text("Line Width")
+				DSFStepperView.SwiftUI(
+					configuration: DSFStepperView.SwiftUI.DisplaySettings(minimum: 1, maximum: 10, initialValue: 1),
+					floatValue: $lineWidth
+				)
+				.frame(width: 100, height: 24)
+			}
+
+			HStack(spacing: 4) {
+				Text("Bar spacing")
+				DSFStepperView.SwiftUI(
+					configuration: DSFStepperView.SwiftUI.DisplaySettings(minimum: 1, maximum: 10, initialValue: 1),
+					floatValue: $barWidth
+				)
+				.frame(width: 100, height: 24)
+			}
+
+			ZeroLineConfigurationView(lineColor: $zerolineColor,
+											  lineWidth: $zerolineWidth,
+											  lineDashStyle: $zeroLineDashStyle)
+				.frame(width: 200)
+
+			DSFSparklineBarGraphView.SwiftUI(
+				dataSource: BarDataSource,
+				graphColor: DSFColor(cgColor: graphColor.cgColor!)!,
+				lineWidth: UInt(lineWidth!),
+				barSpacing: UInt(barWidth!),
+				showZeroLine: showZeroLine,
+				zeroLineDefinition: DSFSparklineZeroLineDefinition(
+					color: DSFColor(zerolineColor),
+					lineWidth: zerolineWidth ?? 0.5,
+					lineDashStyle: zeroLineDashStyle),
+				centeredAtZeroLine: centerAroundZeroLine,
+				lowerGraphColor: DSFColor(lowerGraphColor)
+				//			showHighlightRange: Bool,
+				//			highlightDefinitions: [DSFSparklineHighlightRangeDefinition]
+			)
+			.frame(height: 64)
+			.padding(4)
+			.border(Color.gray.opacity(0.4))
+		}
+		.padding(8)
+		.frame(minWidth: 400)
+	}
+}
+
+extension DSFColor {
+	convenience init(color: Color) {
+		self.init(cgColor: color.cgColor!)!
+	}
+}
+
+struct BarGraphBuilderContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		BarGraphBuilderContentView()
+	}
+}

Demos/macOS SwiftUI Demo/ContentView.swift
@@ -259,9 +259,9 @@ var SparklineView_PreviewGlobalDataSource1: DSFSparklineDataSource = {
 }()
 
 var SparklineView_PreviewGlobalDataSource2: DSFSparklineDataSource = {
-	let d = DSFSparklineDataSource(windowSize: 10, range: -100.0 ... 30.0)
+	let d = DSFSparklineDataSource(windowSize: 10, range: -100.0 ... 100.0)
 	d.push(values: [20, 10, 0, -10, -20, -30, 40, 50, 60, 70])
-	d.zeroLineValue = -80
+	d.zeroLineValue = -20
 	return d
 }()
 

Demos/macOS SwiftUI Demo/ZeroLineConfigurationView.swift
@@ -1,0 +1,90 @@
+//
+//  ZeroLineConfigurationView.swift
+//  macOS SwiftUI Demo
+//
+//  Created by Darren Ford on 13/2/21.
+//
+
+import SwiftUI
+import DSFStepperView
+
+struct ZeroLineConfigurationView: View {
+
+	@Binding var lineColor: Color
+	@Binding var lineWidth: CGFloat?
+
+	@State var onWidth: CGFloat? {
+		didSet {
+			lineDashStyle[0] = self.onWidth!
+		}
+	}
+	@State var offWidth: CGFloat? {
+		didSet {
+			lineDashStyle[1] = self.offWidth!
+		}
+	}
+
+	@Binding var lineDashStyle: [CGFloat]
+
+	var formatter: NumberFormatter = {
+		let format = NumberFormatter()
+		format.numberStyle = .decimal
+
+		// Always display a single digit fractional value.
+		format.allowsFloats = true
+		format.minimumFractionDigits = 1
+		format.maximumFractionDigits = 1
+
+		return format
+	}()
+
+	var body: some View {
+
+		VStack {
+
+			ColorPicker("Zero-line color", selection: $lineColor)
+
+			HStack {
+				Text("Width")
+				DSFStepperView.SwiftUI(
+					configuration: DSFStepperView.SwiftUI.DisplaySettings(minimum: 0.5, maximum: 5.0, increment: 0.5,
+																							initialValue: lineWidth!,
+																							numberFormatter: formatter),
+					floatValue: $lineWidth
+				)
+				.frame(width: 96)
+				DSFStepperView.SwiftUI(
+					configuration: DSFStepperView.SwiftUI.DisplaySettings(minimum: 0.5, maximum: 5.0, increment: 0.5,
+																							initialValue: lineDashStyle[0],
+																							numberFormatter: formatter),
+					floatValue: $onWidth
+				)
+				.frame(width: 96)
+				DSFStepperView.SwiftUI(
+					configuration: DSFStepperView.SwiftUI.DisplaySettings(minimum: 0.5, maximum: 5.0, increment: 0.5,
+																							initialValue: lineDashStyle[1],
+																							numberFormatter: formatter),
+					floatValue: $offWidth
+				)
+				.frame(width: 96)
+			}
+			.frame(height: 26)
+		}
+	}
+}
+
+struct ZeroLineConfigurationView_Previews: PreviewProvider {
+
+	static var lineColor: Color = Color(.sRGB, red: 0.5, green: 0.5, blue: 0.5)
+	static var lineWidth: CGFloat = 1
+	//@State var lineDashStyle: [CGFloat]
+
+	static var previews: some View {
+		//Text("fish")
+		ZeroLineConfigurationView(
+			lineColor: .constant(lineColor),
+			lineWidth: .constant(lineWidth),
+			lineDashStyle: .constant([1, 1])
+		)
+	}
+}

Sources/DSFSparkline/DSFSparklineBarGraphView+SwiftUI.swift
@@ -141,7 +141,9 @@ public extension DSFSparklineBarGraphView.SwiftUI {
 		return self.makeBarGraph(context)
 	}
 
-	func updateUIView(_: DSFSparklineBarGraphView, context _: Context) {}
+	func updateUIView(_ view: DSFSparklineBarGraphView, context _: Context) {
+		self.updateView(view)
+	}
 }
 
 // MARK: - macOS Specific
@@ -152,7 +154,34 @@ public extension DSFSparklineBarGraphView.SwiftUI {
 		return self.makeBarGraph(context)
 	}
 
-	func updateNSView(_: DSFSparklineBarGraphView, context _: Context) {}
+	func updateNSView(_ view: DSFSparklineBarGraphView, context _: Context) {
+		self.updateView(view)
+	}
+}
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, *)
+public extension DSFSparklineBarGraphView.SwiftUI {
+	func updateView(_ view: DSFSparklineBarGraphView) {
+
+		UpdateIfNotEqual(result: &view.graphColor, val: self.graphColor)
+		UpdateIfNotEqual(result: &view.barSpacing, val: self.barSpacing)
+		UpdateIfNotEqual(result: &view.lineWidth, val: self.lineWidth)
+
+		UpdateIfNotEqual(result: &view.showZeroLine, val: self.showZeroLine)
+		view.setZeroLineDefinition(self.zeroLineDefinition)
+
+		UpdateIfNotEqual(result: &view.centeredAtZeroLine, val: self.centeredAtZeroLine)
+		UpdateIfNotEqual(result: &view.lowerGraphColor, val: self.lowerGraphColor)
+
+		if self.highlightDefinitions.count > 0 {
+			view.showHighlightRange = true
+			view.highlightRangeDefinition = self.highlightDefinitions
+		}
+		else {
+			view.showHighlightRange = false
+			view.highlightRangeDefinition = []
+		}
+	}
 }
 
 #endif

Sources/DSFSparkline/DSFSparklineDataBarGraphView+SwiftUI.swift
@@ -125,16 +125,7 @@ public extension DSFSparklineDataBarGraphView.SwiftUI {
 	}
 
 	func updateUIView(_ view: DSFSparklineDataBarGraphView, context _: Context) {
-		view.strokeColor = self.strokeColor
-		view.unsetColor = self.unsetColor
-		view.lineWidth = self.lineWidth
-		view.palette = self.palette
-
-		view.animated = self.animated
-		view.animationDuration = self.animationDuration
-
-		view.dataSource = self.dataSource
-		view.maximumTotalValue = self.maximumTotalValue
+		self.updateView(view)
 	}
 }
 
@@ -147,15 +138,26 @@ public extension DSFSparklineDataBarGraphView.SwiftUI {
 	}
 
 	func updateNSView(_ view: DSFSparklineDataBarGraphView, context _: Context) {
-		view.strokeColor = self.strokeColor
-		view.unsetColor = self.unsetColor
-		view.lineWidth = self.lineWidth
-		view.palette = self.palette
+		self.updateView(view)
+	}
+}
 
-		view.animated = self.animated
-		view.animationDuration = self.animationDuration
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, *)
+public extension DSFSparklineDataBarGraphView.SwiftUI {
+	func updateView(_ view: DSFSparklineDataBarGraphView) {
+		UpdateIfNotEqual(result: &view.strokeColor, val: self.strokeColor)
+		UpdateIfNotEqual(result: &view.unsetColor, val: self.unsetColor)
+		
+		UpdateIfNotEqual(result: &view.lineWidth, val: self.lineWidth)
 
-		view.dataSource = self.dataSource
+		UpdateIfNotEqual(result: &view.palette, val: self.palette)
+
+		UpdateIfNotEqual(result: &view.animated, val: self.animated)
+		UpdateIfNotEqual(result: &view.animationDuration, val: self.animationDuration)
+
+		UpdateIfNotEqual(result: &view.dataSource, val: self.dataSource)
+
+		UpdateIfNotEqual(result: &view.maximumTotalValue, val: self.maximumTotalValue)
 	}
 }
 

Sources/DSFSparkline/private/ui/DSFSparklineBarGraphView+Private.swift
@@ -90,16 +90,18 @@ public extension DSFSparklineBarGraphView {
 				outer.clip(to: clipRect)
 			}
 
-			let path = CGMutablePath()
+			var bars: [CGRect] = []
 			for point in points.enumerated() {
 				let yVal = Int(point.element.y.rounded(.down))
 				let r = CGRect(x: xOffset + point.offset * componentWidth,
 									y: Int(integralRect.height) - yVal,
 									width: barWidth,
 									height: yVal - Int(self.lineWidth))
-				path.addRect(r.integral)
+				bars.append(r.integral)
 			}
-			path.closeSubpath()
+
+			let path = CGMutablePath()
+			path.addRects(bars)
 
 			outer.addPath(path)
 			outer.setFillColor(self.graphColor.withAlphaComponent(0.3).cgColor)
@@ -136,8 +138,8 @@ public extension DSFSparklineBarGraphView {
 				outer.clip(to: clipRect)
 			}
 
-			let positivePath = CGMutablePath()
-			let negativePath = CGMutablePath()
+			var positivePath: [CGRect] = []
+			var negativePath: [CGRect] = []
 
 			for value in normy.enumerated() {
 				let x = CGFloat(value.offset) * xDiff
@@ -147,7 +149,7 @@ public extension DSFSparklineBarGraphView {
 										y: centroid,
 										width: xDiff - 1 - (CGFloat(self.barSpacing)),
 										height: yy) // - CGFloat(self.lineWidth))
-					positivePath.addRect(r.integral)
+					positivePath.append(r.integral)
 				}
 				else {
 					let yy = (value.element - centre) * height
@@ -155,17 +157,17 @@ public extension DSFSparklineBarGraphView {
 										y: centroid,
 										width: xDiff - 1 - (CGFloat(self.barSpacing)),
 										height: -yy - CGFloat(self.lineWidth))
-					negativePath.addRect(r.integral)
+					negativePath.append(r.integral)
 				}
 			}
-			positivePath.closeSubpath()
-			negativePath.closeSubpath()
 
 			outer.setShouldAntialias(false)
 
 			if !positivePath.isEmpty {
+				let path = CGMutablePath()
+				path.addRects(positivePath)
 				outer.usingGState { ctx in
-					ctx.addPath(positivePath)
+					ctx.addPath(path)
 					ctx.setFillColor(self.graphColor.withAlphaComponent(0.3).cgColor)
 					ctx.setLineWidth(1 / self.retinaScale() * CGFloat(self.lineWidth))
 					ctx.setStrokeColor(self.graphColor.cgColor)
@@ -174,8 +176,10 @@ public extension DSFSparklineBarGraphView {
 			}
 
 			if !negativePath.isEmpty {
+				let path = CGMutablePath()
+				path.addRects(negativePath)
 				outer.usingGState { ctx in
-					ctx.addPath(negativePath)
+					ctx.addPath(path)
 					ctx.setFillColor(self.lowerColor.withAlphaComponent(0.3).cgColor)
 					ctx.setLineWidth(1 / self.retinaScale() * CGFloat(self.lineWidth))
 					ctx.setStrokeColor(self.lowerColor.cgColor)

commit 6daac5156f46c3e043e67bdf5df0ae338ff8d4c9

Fixed bug in drawing unset values in dot graph introduced in d431c44

Sources/DSFSparkline/private/ui/DSFSparklineDotGraphView+Private.swift
@@ -63,8 +63,6 @@ private extension DSFSparklineDotGraphView {
 
 		var position = drawRect.width - dotHeight - xOffset
 
-		let unsetPath = CGMutablePath()
-
 		// Map normalized values to box positions
 		let normalizedBoxed: [UInt] = dataSource.normalized.reversed().map { dataPoint in
 			let floatBoxPos = CGFloat(self.verticalDotCount) * dataPoint
@@ -86,11 +84,9 @@ private extension DSFSparklineDotGraphView {
 
 				if c < boxCount {
 					pv.append(ri)
-					//path.addRect(ri)
 				}
 				else {
 					uv.append(ri)
-					//unsetPath.addRect(ri)
 				}
 			}
 
@@ -115,7 +111,7 @@ private extension DSFSparklineDotGraphView {
 			primary.usingGState { (state) in
 				let path = CGMutablePath()
 				path.addRects(uv)
-				state.addPath(unsetPath)
+				state.addPath(path)
 				state.setFillColor(self.unsetGraphColor.cgColor)
 				state.drawPath(using: .fill)
 			}