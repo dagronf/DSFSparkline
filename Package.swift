1bed49d43e58be201bfc3b0b4045f580a3b26e9b
Darren Ford <dford_au-reg@yahoo.com>
2022-11-08 07:20:33 +1100

    Default to using non-specified library type for DSFSparkline
    See: https://github.com/dagronf/DSFSparkline/issues/10

--- Package.swift
+++ Package.swift
@@ -3,25 +3,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "DSFSparkline",
-    platforms: [
-       .macOS(.v10_11),
-       .iOS(.v11),
-       .tvOS(.v11)
-    ],
-    products: [
-        .library(name: "DSFSparkline", type: .static, targets: ["DSFSparkline"]),
-		  .library(name: "DSFSparkline-shared", type: .dynamic, targets: ["DSFSparkline"]),
-    ],
-    dependencies: [
-    ],
-    targets: [
-        .target(
-            name: "DSFSparkline",
-            dependencies: []),
-        .testTarget(
-            name: "DSFSparklineTests",
-            dependencies: ["DSFSparkline"]),
-    ],
-	 swiftLanguageVersions: [.v5]
+	name: "DSFSparkline",
+	platforms: [
+		.macOS(.v10_11),
+		.iOS(.v11),
+		.tvOS(.v11)
+	],
+	products: [
+		.library(name: "DSFSparkline", targets: ["DSFSparkline"]),
+		.library(name: "DSFSparkline-static", type: .static, targets: ["DSFSparkline"]),
+		.library(name: "DSFSparkline-shared", type: .dynamic, targets: ["DSFSparkline"]),
+	],
+	dependencies: [
+	],
+	targets: [
+		.target(
+			name: "DSFSparkline",
+			dependencies: []),
+		.testTarget(
+			name: "DSFSparklineTests",
+			dependencies: ["DSFSparkline"]),
+	],
+	swiftLanguageVersions: [.v5]
 )
